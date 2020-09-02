1b61340b6e46020905f1c8154bfa5a2c7023c0aa
Sheikh Muhammed Tadeeb <70131688+smTadeeb@users.noreply.github.com>
2020-09-02 22:00:16 +0530

    Added Mouse-click functionality to the game
    removed grid made using create_lines()

--- Habibi/Habibi.py
+++ Habibi/Habibi.py
@@ -31,13 +31,7 @@ L3.pack( side = TOP , anchor = "s" ,fill= X,pady=3,padx=3)
 
 can_widget = Canvas(myInterface, width=500, height=500, background= "gold")       # Created Canvas         
 can_widget.pack(fill = Y)
-
-can_widget.create_line(0,5,500,5,fill = "black",width = 4)                       #####################################
-can_widget.create_line(5,5,5,500,fill = "black",width = 4)                       ##                                 ##
-can_widget.create_line(5,500,500,500,fill = "black",width = 4)                   ## Created canvas(GUI) and Borders ##
-can_widget.create_line(500,5,500,500,fill = "black",width = 4)                   ##                                 ##
-                                                                                 #####################################
-                                                                                 
+                                                                                
 def close_window():
     """
     Asks the user if they really want to exit
@@ -45,7 +39,6 @@ def close_window():
     if tkinter.messagebox.askokcancel("Exit", "Do you really wish to Exit?"):
         myInterface.destroy()
    
-
 def game_rules():
     """
     Displays game rules
@@ -62,21 +55,7 @@ def restart_program():
     python = sys.executable
     os.execl(python, python, * sys.argv)
     return restart_program()
-    
-def checkered(canvas, line_distance):  
-    """
-    Used to make the 10*10 grid on canvas
-    """
-                                                                                           ########################################################
-    for x in range(line_distance,500,line_distance):                                       ##                                                    ##
-       canvas.create_line(x, 0, x, 500)                                                    ## Vertical lines at an interval of "line_distance"   ##
-                                                                                           ## Horizontal lines at an interval of "line_distance" ##
-    for y in range(line_distance,500,line_distance):                                       ##                                                    ##
-       canvas.create_line(0, y, 500, y)                                                    ########################################################                                                      
-                                                                              
-
-    
-    
+                                                                                                                                   
 b1 = Button(f1,fg="red",text="Exit",padx = "8",pady = "8",command = close_window)            #################################################################
 b1.pack(side = TOP ,anchor ="s")                                                             ##                                                             ##
                                                                                              ## Buttons on canvas to exit,restart and to check game rules.  ##                 
@@ -88,7 +67,132 @@ b3.pack(side = LEFT ,anchor = "sw" )
 
 
 checkered(can_widget,50)
-myInterface.mainloop()
+
+#Board Size
+Board_Size = int(16.2)
+Frame_Gap = 35
+width = 500
+height = 500
+
+def create_circle(x, y, radius, fill = "", outline = "black", width = 1):
+    can_widget.create_oval(x - radius, y - radius, x + radius, y + radius, fill = fill, outline = outline, width = width)
+
+def Value_Check_int(Value):
+    try:
+        Value = int(Value)
+    except ValueError:
+        return "string"
+    else:
+        return "int"
+
+def MouseClick(event):
+    global Click_Cord
+    X_click = event.x
+    Y_click = event.y
+    Click_Cord = Piece_Location(X_click, Y_click)
+    print(Click_Cord)
+
+can_widget.bind("<Button-1>", MouseClick)
+
+Click_Cord = [None, None]
+
+def Piece_Location(X_click, Y_click):    
+    X = None
+    Y = None
+    for i in range(len(Actual_CordX1)):
+        
+        if X_click > Actual_CordX1[i] and X_click < Actual_CordX2[i]:
+            X = Game_CordX[i]
+
+        if Y_click > Actual_CordY1[i] and Y_click < Actual_CordY2[i]:
+            Y = Game_CordY[i]
+
+    return X, Y
+
+def Location_Validation():
+
+    if X == None or Y == None:
+        return False
+        
+    elif board[Y - 1][X - 1] == 0:
+        return True
+
+#Board
+Board_Size = Board_Size - 1
+Board_X1 = 500 / 10
+Board_Y1 = 500 / 10
+Board_GapX = (500 - Board_X1 * 2) / Board_Size
+Board_GapY = (500 - Board_Y1 * 2) / Board_Size
+
+#Chess Piece
+Chess_Radius = (Board_GapX * (9 / 10)) / 2
+
+#Cord List
+Black_Cord_PickedX = []
+Black_Cord_PickedY = []
+White_Cord_PickedX = []
+White_Cord_PickedY = []
+
+#Click Detection Cord
+Game_CordX = []
+Game_CordY = []
+Actual_CordX1 = []
+Actual_CordY1 = []
+Actual_CordX2 = []
+Actual_CordY2 = []
+
+#2D Board List
+board = []
+
+#2D list for gameboard
+for i in range(Board_Size + 1):
+    board.append([0] * (Board_Size + 1))
+    
+Unfilled = 0
+Black_Piece = 1
+White_Piece = 2
+
+#Fills Empty List
+for z in range(1, Board_Size + 2):
+    
+    for i in range(1, Board_Size + 2):
+        Game_CordX.append(z)
+        Game_CordY.append(i)
+        Actual_CordX1.append((z - 1) * Board_GapX + Board_X1 - Chess_Radius)
+        Actual_CordY1.append((i - 1) * Board_GapY + Board_Y1 - Chess_Radius)
+        Actual_CordX2.append((z - 1) * Board_GapX + Board_X1 + Chess_Radius)
+        Actual_CordY2.append((i - 1) * Board_GapY + Board_Y1 + Chess_Radius)
+        
+#Turn
+Turn_Num = 1
+Turn = "black"
+Winner = None
+
+#Game Code
+while Winner == None:
+    can_widget.update()
+
+    X = Click_Cord[0]
+    Y = Click_Cord[1]
+
+    Picked = Location_Validation()
+
+    if Picked:       
+        create_circle(Board_X1 + Board_GapX * (X - 1), Board_Y1 + Board_GapY * (Y - 1), radius = Chess_Radius, fill = Turn)
+
+        if Turn_Num % 2 == 1:
+            White_Cord_PickedX.append(X)
+            White_Cord_PickedY.append(Y)
+            board[Y - 1][X - 1] = 2
+            Turn = "black"
+
+        elif Turn_Num % 2 == 0:
+            Black_Cord_PickedX.append(X)
+            Black_Cord_PickedY.append(Y)
+            board[Y - 1][X - 1] = 1
+            Turn = "white"
+      
+myInstance.mainloop()
 
 
 
